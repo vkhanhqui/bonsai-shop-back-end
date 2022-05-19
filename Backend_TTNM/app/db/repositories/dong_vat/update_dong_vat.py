8d86d7d2f19ea4fdbe3fd28232fa36b9bd4dae71
Steve <vkhanhqui@gmail.com>
2022-05-19 22:51:29 +0700

    Bugfix API update animal

--- Backend_TTNM/app/db/repositories/dong_vat/update_dong_vat.py
+++ Backend_TTNM/app/db/repositories/dong_vat/update_dong_vat.py
@@ -11,9 +11,26 @@ def update_dong_vat(_in: DongVatUpdate):
         db.query(DongVat).\
         filter(DongVat.id_dong_vat == _in.id_dong_vat).\
         update({
-                DongVat.id_user: _in.id_user,
-                DongVat.id_gia_tri: _in.id_gia_tri
-            })
+            DongVat.id_user: _in.id_user,
+            DongVat.id_gioi: _in.id_gioi,
+            DongVat.id_nganh: _in.id_nganh,
+            DongVat.id_lop: _in.id_lop,
+            DongVat.id_bo: _in.id_bo,
+            DongVat.id_ho: _in.id_ho,
+            DongVat.id_gia_tri: _in.id_gia_tri,
+            DongVat.id_tinh_trang_bao_ton: _in.id_tinh_trang_bao_ton,
+            DongVat.id_tinh_trang_mau_vat: _in.id_tinh_trang_mau_vat,
+            DongVat.id_phan_bo: _in.id_phan_bo,
+            DongVat.id_sinh_canh: _in.id_sinh_canh,
+            DongVat.dia_diem: _in.dia_diem,
+            DongVat.ngay_thu_mau: _in.ngay_thu_mau,
+            DongVat.hinh_thai: _in.hinh_thai,
+            DongVat.sinh_thai: _in.sinh_thai,
+            DongVat.ten_dia_phuong: _in.ten_dia_phuong,
+            DongVat.ten_khoa_hoc: _in.ten_khoa_hoc,
+            DongVat.ten_tieng_viet: _in.ten_tieng_viet,
+            DongVat.nguoi_thu_mau: _in.nguoi_thu_mau,
+        })
         db.commit()
         return _in
     except:
